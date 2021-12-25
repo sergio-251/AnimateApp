6d8db72b8a2f2c732bfa33066fa583814402de88
EfimovSchool <sergio-251@yandex.ru>
2021-12-25 13:23:56 +0300

    Initial Commit

---
+++ AnimateApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimateApp
+//
+//  Created by Sergey Efimov on 25.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
